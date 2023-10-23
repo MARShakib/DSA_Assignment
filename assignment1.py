2b51599da7257e86e2920d5f6f0796ab98bba633
Md Ashikur Rahman <arshakib51@gmail.com>
2023-10-23 17:33:21 +0600

    Add length() and find_middle() methods

--- assignment1.py
+++ assignment1.py
@@ -39,6 +39,26 @@ class LinkedList:
             print(cur.data, end=" ")
             cur = cur.next
 
+    ## 1. Find the length of the linked list.
+    def length(self):
+        count = 0
+        cur = self.head
+        while cur:
+            count += 1
+            cur = cur.next
+        return count
+
+    ## 2. Find the middle element of the list.
+    def find_middle(self):
+        mid_len = self.length() // 2
+        middle = self.head
+        while mid_len > 0:
+            mid_len -= 1
+            middle = middle.next
+        if not middle:
+            return "Empty list. No middle value."
+        return middle.data
+
 
 linked_list = LinkedList()
 linked_list.insert(5)
@@ -46,7 +66,10 @@ linked_list.insert(3)
 linked_list.insert(7)
 linked_list.insert(2)
 linked_list.insert(8)
-
-linked_list.delete(5)
-
 linked_list.print_list()
+print(f"\nLength:{linked_list.length()}")
+print(f"Middle:{linked_list.find_middle()}")
+linked_list.delete(5)
+linked_list.print_list()
+print(f"\nLength:{linked_list.length()}")
+print(f"Middle:{linked_list.find_middle()}")
